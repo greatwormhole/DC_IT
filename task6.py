2464bb7736741a3971c8e4e03527722260a32e57
Nikita Chistyakov <nikita.chistyakov2002@gmail.com>
2022-10-12 22:31:08 +0300

    Optimized #6, 7, 8

--- task6.py
+++ task6.py
@@ -1,9 +1,8 @@
-l = input().lower().split(sep=", ")
-s = list(set(l))
-s.sort()
-k = len(s)
+l = list(set(input().lower().split(sep=", ")))
+l.sort()
+k = len(l)
 for i in range(k):
     if i == k-1:
-        print(s[i])
+        print(l[i])
     else:
-        print(s[i], end=", ")
+        print(l[i], end=", ")
--- task7.py
+++ task7.py
@@ -1,10 +1,7 @@
 s = input().split(sep=", ")
 d = {}
 for word in s:
-    if word not in list(d.keys()):
-        d.update({word: 1})
-    else:
-        d[word] += 1
+    d[word] = d.get(word, 0) + 1
 sorted_keys = sorted(d, key=d.get, reverse=True)
 sorted_dict = {}
 for i in sorted_keys:
