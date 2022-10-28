2bc8e6a6c3cb864025c79072a02b7cfc56fe9442
Nikita Chistyakov <nikita.chistyakov2002@gmail.com>
2022-10-29 01:35:54 +0300

    Optimized task #9

--- task9.py
+++ task9.py
@@ -1,14 +1,14 @@
 import datetime
 
 def gift_count(budget, month, birthdays):
-    res = [(i, '(' + j.strftime("%d.%m.%Y") + ')') for i, j in filter(lambda x: x[-1].month == month, birthdays.items())]
-    print(f"Именинники в месяце {month}:", end=' ')
-    for i, j in enumerate(res):
-        if i == 0:
-            print(*j, sep=' ', end='')
-        else:
-            print(',', *j, sep=' ', end='')
-    print(f". При бюджете {budget} они получат по {budget // len(res)} рублей.")
+    res = sorted([f"{i} ({j.strftime('%d.%m.%Y')})"
+                for i, j in filter(lambda x: x[-1].month == month, birthdays.items())],
+                key=lambda x: x[-1])
+    if len(res) != 0:
+        print(f"Именинники в месяце {month}: {', '.join(res)}"
+              f". При бюджете {budget} они получат по {budget // len(res)} рублей.")
+    else:
+        print("В этом месяце нет именниннков.")
 
 if __name__ == "__main__":
     birthdays = {"Иванов Иван Иванович": datetime.date(1989, 5, 1), "Петров Петр Петрович": datetime.date(1998, 5, 6)}
