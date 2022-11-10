3f82e9e1f0f7c36724d2ddf55082da5d3bd3a151
Nikita Chistyakov <nikita.chistyakov2002@gmail.com>
2022-11-10 07:33:24 +0300

    Created template for task#16

---
+++ CompletedTasks/week05/util.py
@@ -0,0 +1,15 @@
+class Calculator:
+    def sum(self, a: int | float, b: int | float) -> int | float:
+        pass
+    def sub(self, a: int | float, b: int | float) -> int | float:
+        pass
+    def mul(self, a: int | float, b: int | float) -> int | float:
+        pass
+    def div(self, a: int | float, b: int | float, mode: bool = False) -> int | float:
+        pass
+    def history(self, n: int) -> int | float:
+        pass
+    def last():
+        pass
+    def clear(cls):
+        pass
