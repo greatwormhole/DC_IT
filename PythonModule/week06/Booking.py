601d468a2189fe0defe8c55852e2f6024cd8d88c
Nikita Chistyakov <nikita.chistyakov2002@gmail.com>
2022-11-20 02:10:25 +0300

    Completed task#20

--- PythonModule/week06/Booking.py
+++ PythonModule/week06/Booking.py
@@ -1,2 +1,58 @@
+import datetime
+import json
+from api import register_booking
+
 class Booking:
-    pass
+    
+    def __init__(self, __name: str, __start: datetime.datetime, __end: datetime.datetime) -> None:
+        if __start > __end:
+            raise ValueError
+        self.room_name = __name
+        self.start = __start
+        self.end = __end
+
+    @property
+    def duration(self):
+        return (self.end - self.start).total_seconds() // 60
+
+    @property
+    def start_date(self):
+        return self.start.strftime("%Y-%m-%d")
+
+    @property
+    def end_date(self):
+        return self.end.strftime("%Y-%m-%d")
+
+    @property
+    def start_time(self):
+        return self.start.strftime("%H:%M")
+    
+    @property
+    def end_time(self):
+        return self.end.strftime("%H:%M")
+
+def create_booking(room_name, start, end) -> str:
+    print("Начинаем создание бронирования")
+    res = {}
+    try:
+        bkg = Booking(room_name, start, end)
+        res["booking"] = {"room_name": bkg.room_name,
+                            "start_date": bkg.start_date,
+                            "start_time": bkg.start_time,
+                            "end_date": bkg.end_date,
+                            "end_time": bkg.end_time,
+                            "duration": bkg.duration}
+        if register_booking(bkg) == True:
+            res["created"] = True
+            res["msg"] = "Бронирование создано"
+        else:
+            res["created"] = False
+            res["msg"] = "Комната занята"
+    except ValueError:
+        return None
+    except KeyError:
+        res["created"] = False
+        res["msg"] = "Комната не найдена"
+    finally:
+        print("Заканчиваем создание бронирования")
+    return json.dumps(res)
